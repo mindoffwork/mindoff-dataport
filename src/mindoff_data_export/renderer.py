0cc614dfecad66ed82694dcc10a2cefd753b9def
Joel Samraj <joelsamrajs@gmail.com>
2026-04-23 15:14:03 +0530

    Revert ":sparkles: Support safe streaming merges and lazy header schema"
    This reverts commit fc1eeda2ded56bc80a68a2119a64f78a602e777c.

--- src/mindoff_data_export/renderer.py
+++ src/mindoff_data_export/renderer.py
@@ -418,7 +418,7 @@ def _to_headers(df_or_headers: Any) -> list[str]:
 
     if "polars" in module:
         if qualname == "LazyFrame":
-            return [str(col) for col in df_or_headers.collect_schema().names()]
+            df_or_headers = df_or_headers.collect()
         return [str(col) for col in df_or_headers.columns]
 
     if "pandas" in module and "DataFrame" in qualname:
